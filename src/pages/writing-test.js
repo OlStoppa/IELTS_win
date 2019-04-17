7730ee7b475e83c97e9c38fd06c5566e17188587
OlStoppa <olthespud@hotmail.com>
2019-04-18 00:15:25 +0800

    styled writing test

--- src/pages/writing-test.js
+++ src/pages/writing-test.js
@@ -2,46 +2,57 @@ import React from "react"
 import Header from "../components/header"
 import Menu from "../components/Menu"
 
+const questions = [
+  [
+    <p>
+      Car ownership has increased so rapidly over the past thirty years that
+      many cities in the world have become one big traffic jam.
+    </p>,
+    <p>How true do you think this statement is?</p>,
+    <p>
+      What measures can governments take to discourage people from using their
+      cars?
+    </p>,
+  ],
+  [
+    <p>Government investment in the arts, such as music and theatre, is a waste of money. Governments must invest this money in public services instead.</p>,
+    <p>To what extent do you agree with this statement?</p>
+  ],
+  [
+    <p>Some people believe the aim of university education is to help graduates get better jobs. Others believe there are much wider benefits of university education for both individuals and society.</p>,
+    <p>Discuss both views and give your opinion.</p>
+  ]
+]
+
 class WritingTest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: 0
+    };
+
+    this.handleQuestionChange = this.handleQuestionChange.bind(this);
+  }
+  
+  handleQuestionChange(){
+    this.setState((state) => {
+     return { question: state.question === questions.length - 1 ? 0 : state.question + 1 }
+            })
+  }
   render() {
     return (
       <div>
         <Header siteTitle="IELTS WIN" />
         <Menu />
-        <div
-          style={{
-            width: "100vw",
-            height: "100vh",
-            backgroundColor: "#ADD5FF",
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            
-          }}
-        >
-          <div style={{ borderWidth: 2, borderColor: "black", width: "100%" }}>
-            <div
-              style={{
-                width: "50%",
-                background: "white",
-                borderRadius: 50,
-                height: 250,
-                padding: 20,
-              }}
-            >
-              <p>
-                Car ownership has increased so rapidly over the past thirty
-                years that many cities in the world have become one big traffic
-                jam.
-              </p>
-              <p>How true do you think this statement is?</p>
-              <p>
-                What measures can governments take to discourage people from
-                using their cars?
-              </p>
+        <div className="writing-page--body">
+          <div className="test-container">
+          <button onClick={this.handleQuestionChange}>change question</button>
+            <div className="test-question" >
+            {questions[this.state.question]}
             </div>
-            <div>
-              <textarea />
+            <div className="answer-container">
+              <textarea className="text-area__writing-test" />
+              
             </div>
           </div>
         </div>
