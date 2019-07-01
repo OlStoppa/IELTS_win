7db0847b32a40e603f3f69878817d17d04a2a1a1
OlStoppa <olthespud@hotmail.com>
2019-07-01 22:14:09 +0800

    wrote app privacy policy

---
+++ src/pages/grademyieltsspeaking-privacy.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import BareLayout from '../components/bareLayout'
+
+const AppPrivacy = () => (
+    <BareLayout>
+        <div className="terms-page">
+            <div className="terms-page__header">
+                <h1>Privacy Policy</h1>
+            </div>
+            <div className="terms-page__body">
+            <p>Oliver Lennon built the Grade My IELTS Speaking app as a Free app. This SERVICE is provided by Oliver Lennon at no cost and is intended for use as is</p>
+			<p>The grading service is provided via in-app purchase and requires me to collect a user email and the relevant audio files created while using the app. If, and only if you choose to use this service, the selected files and email address will be stored using secure Google services and used only for providing a specific user with feedback and score information. This information will not be shared with anyone and will be deleted as soon as feedback has been provided.</p>
+			<h3>Log Data</h3>
+            <p>I want to inform you that whenever you use my Service, in a case of an error in the app I collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics</p>		
+            <h3>Security</h3>
+            <p>I value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its absolute security.</p>
+            <h3>Links to Other Sites</h3>
+            <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+            <h3>Children’s Privacy</h3>
+            <p>I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.</p>
+            <h3>Contact Us</h3>
+            <p>If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at oliver@ieltsxl.com.</p>
+            </div>
+
+        </div>
+    </BareLayout>
+)
+
+export default AppPrivacy
