d998f3279ef2489efb79692daea578435621e42b
OlStoppa <olthespud@hotmail.com>
2019-07-03 12:30:39 +0800

    updated contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,14 +9,14 @@ const ContactPage = () => (
       <div className="contact-page__body">
           <div className="contact-page__image--container">
               <h4>Please contact us with any questions about our services or requests for one to one skype lessons.</h4>
-            <h4>We will get back to you by e-mail as soon as possible.</h4>
+            <h4>We will get back to you by email as soon as possible.</h4>
           </div>
           <div className="contact-page__form--container">
-              <form>
-                  <input placeholder="Name"/>
-                  <input placeholder="Your Email"/>
-                  <textarea placeholder="Message"/>
-                  <button>SUBMIT</button>
+              <form name="contact" method="POST" data-netlify="true">
+                  <input placeholder="Name" type="text" name="name"/>
+                  <input placeholder="Your Email" type="email" name="email"/>
+                  <textarea placeholder="Message" name="message"/>
+                  <button type="submit">SUBMIT</button>
               </form>
           </div>
       </div>
